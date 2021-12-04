542698e65ae777e93291204d001571f8bbdb290b
yuncheng <h1253567@gmail.com>
2021-12-04 15:32:46 +0800

    testing get user Profile

--- fast-tracking-line-bot/hook/handleFollow.js
+++ fast-tracking-line-bot/hook/handleFollow.js
@@ -6,10 +6,7 @@ const handleFollow = async (event, replyToken) => {
   client
     .getProfile(event.source.userId)
     .then((profile) => {
-      console.log(profile.displayName); //顯示使用者名字
-      console.log(profile.userId);
-      console.log(profile.pictureUrl); // 顯示使用者大頭照網址
-      console.log(profile.statusMessage); // 使用者自介內容
+      console.log(profile);
     })
     .catch((err) => {
       // error handling
