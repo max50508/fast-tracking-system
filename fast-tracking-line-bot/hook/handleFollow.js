9ccc689f9950edcbed3badb9076b0b15891caeb7
yuncheng <h1253567@gmail.com>
2021-12-04 15:42:49 +0800

    testing get user Profile

--- fast-tracking-line-bot/hook/handleFollow.js
+++ fast-tracking-line-bot/hook/handleFollow.js
@@ -3,21 +3,19 @@
 const client = require("../config/client");
 
 const handleFollow = async (event, replyToken) => {
-  const userProfile = {};
   client
     .getProfile(event.source.userId)
     .then((profile) => {
       console.log(profile);
-      userProfile = profile;
+      return await client.replyMessage(replyToken, {
+        type: "text", // ①
+        text: `您好! ${profile.displayName} \n 歡迎加入疫大師共同防疫的行列!`,
+      });
     })
     .catch((err) => {
       // error handling
       console.log(err);
     });
-  return await client.replyMessage(replyToken, {
-    type: "text", // ①
-    text: `您好! ${userProfile.displayName} \n 歡迎加入疫大師共同防疫的行列!`,
-  });
 };
 
 module.exports = handleFollow;
