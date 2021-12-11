e492032e0dc679c0c6264e41a8449095683769a3
yuncheng <h1253567@gmail.com>
2021-12-11 18:54:44 +0800

    update follow friend message

--- fast-tracking-line-bot/hook/handleFollow.js
+++ fast-tracking-line-bot/hook/handleFollow.js
@@ -15,10 +15,30 @@ const handleFollow = async (event, replyToken) => {
       // error handling
       console.log(err);
     });
-  return await client.replyMessage(replyToken, {
-    type: "text", // ①
-    text: `您好! ${userProfile?.displayName} \n 歡迎加入疫大師共同防疫的行列!`,
-  });
+  return await client.replyMessage(replyToken, [
+    {
+      type: "text", // ①
+      text: `你好! ${userProfile?.displayName}, 我是疫大師!\n歡迎你成為我的好友~ $`,
+      emojis: [
+        {
+          index: 0,
+          productId: "5ac1bfd5040ab15980c9b435",
+          emojiId: "038",
+        },
+      ],
+    },
+    {
+      type: "text", // ①
+      text: `當你需要疫情相關資訊時，我們都可以提供給你~`,
+    },
+    {
+      type: "image",
+      originalContentUrl:
+        "https://firebasestorage.googleapis.com/v0/b/yatspic/o/YatsLogo.jpg?alt=media&token=167de9ee-dbce-4ccd-86fd-8b3ca9d81142",
+      previewImageUrl:
+        "https://firebasestorage.googleapis.com/v0/b/yatspic/o/YatsLogo.jpg?alt=media&token=167de9ee-dbce-4ccd-86fd-8b3ca9d81142",
+    },
+  ]);
 };
 
 module.exports = handleFollow;
