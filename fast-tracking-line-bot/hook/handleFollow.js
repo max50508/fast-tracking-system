c8908c672173da7be08323b165e745bd732b68e9
yuncheng <h1253567@gmail.com>
2021-12-11 19:10:13 +0800

    update follow friend message

--- fast-tracking-line-bot/hook/handleFollow.js
+++ fast-tracking-line-bot/hook/handleFollow.js
@@ -19,6 +19,10 @@ const handleFollow = async (event, replyToken) => {
     {
       type: "text", // ①
       text: `你好! ${userProfile?.displayName}, 我是疫大師!\n歡迎你成為我的好友~ $`,
+      emojis: {
+        productId: "5ac1bfd5040ab15980c9b435",
+        emojiId: "038",
+      },
     },
     {
       type: "text", // ①
