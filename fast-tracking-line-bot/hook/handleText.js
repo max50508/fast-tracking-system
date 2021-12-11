57dd7d8b69d0323cf2941b8b9eef8fb4a318c84c
yuncheng <h1253567@gmail.com>
2021-12-11 20:17:03 +0800

    update follow friend message

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -4,43 +4,48 @@ const handleText = async (message, replyToken, source) => {
   switch (message.text) {
     case "你好啊!":
       return await client.replyMessage(replyToken, {
-        type: "template",
-        altText: "疫大師相關注意事項同意書",
-        template: {
-          type: "bubble",
-          body: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "為了提供個人化的服務，我會 紀錄你告訴我的個人資訊，請 詳細閱讀以上聲明，同意後我 就能夠為你進行各項服務！",
-                weight: "regular",
-                size: "lg",
-                gravity: "top",
-                wrap: true,
-                contents: [],
+        type: "flex",
+        altText: "恭喜您解鎖",
+        contents: {
+          type: "carousel",
+          contents: [
+            {
+              type: "bubble",
+              body: {
+                type: "box",
+                layout: "vertical",
+                contents: [
+                  {
+                    type: "text",
+                    text: "為了提供個人化的服務，我會 紀錄你告訴我的個人資訊，請 詳細閱讀以上聲明，同意後我 就能夠為你進行各項服務！",
+                    weight: "regular",
+                    size: "lg",
+                    gravity: "top",
+                    wrap: true,
+                    contents: [],
+                  },
+                ],
               },
-            ],
-          },
-          footer: {
-            type: "box",
-            layout: "vertical",
-            flex: 0,
-            spacing: "sm",
-            contents: [
-              {
-                type: "button",
-                action: {
-                  type: "uri",
-                  label: "閱讀疫大師注意事項及個人資訊保護法相關事",
-                  uri: "https://linecorp.com",
-                },
-                height: "sm",
-                style: "link",
+              footer: {
+                type: "box",
+                layout: "vertical",
+                flex: 0,
+                spacing: "sm",
+                contents: [
+                  {
+                    type: "button",
+                    action: {
+                      type: "uri",
+                      label: "閱讀疫大師注意事項及個人資訊保護法相關事",
+                      uri: "https://linecorp.com",
+                    },
+                    height: "sm",
+                    style: "link",
+                  },
+                ],
               },
-            ],
-          },
+            },
+          ],
         },
       });
     case "人流資訊":
