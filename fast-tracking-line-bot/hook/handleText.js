a1b60e14b7ddcaaabfc54d00f9b72a989359079a
yuncheng <h1253567@gmail.com>
2021-12-11 20:13:00 +0800

    update follow friend message

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -2,30 +2,45 @@ const client = require("../config/client");
 
 const handleText = async (message, replyToken, source) => {
   switch (message.text) {
-    case "送禮自用兩相宜":
+    case "你好啊!":
       return await client.replyMessage(replyToken, {
-        type: "text", // ①
-        text: "請選擇想要的店家類別",
-        quickReply: {
-          // ②
-          items: [
-            {
-              type: "action",
-              action: {
-                type: "message",
-                label: "送禮/自用",
-                text: "送禮/自用",
+        type: "template",
+        altText: "疫大師相關注意事項同意書",
+        template: {
+          type: "bubble",
+          body: {
+            type: "box",
+            layout: "vertical",
+            contents: [
+              {
+                type: "text",
+                text: "為了提供個人化的服務，我會 紀錄你告訴我的個人資訊，請 詳細閱讀以上聲明，同意後我 就能夠為你進行各項服務！",
+                weight: "regular",
+                size: "lg",
+                gravity: "top",
+                wrap: true,
+                contents: [],
               },
-            },
-            {
-              type: "action",
-              action: {
-                type: "uri",
-                label: "周邊店家",
-                uri: "https://line.me/R/nv/location/",
+            ],
+          },
+          footer: {
+            type: "box",
+            layout: "vertical",
+            flex: 0,
+            spacing: "sm",
+            contents: [
+              {
+                type: "button",
+                action: {
+                  type: "uri",
+                  label: "閱讀疫大師注意事項及個人資訊保護法相關事",
+                  uri: "https://linecorp.com",
+                },
+                height: "sm",
+                style: "link",
               },
-            },
-          ],
+            ],
+          },
         },
       });
     case "人流資訊":
