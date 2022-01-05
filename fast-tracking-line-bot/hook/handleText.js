e4cb443189725ac10759cad7a040fb35869f78c6
yuncheng <h1253567@gmail.com>
2022-01-05 23:28:21 +0800

    update 同意

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -1,6 +1,6 @@
 const client = require("../config/client");
 const checkMessage = require("../util/message.js");
-const firebaseStoreDB = require("../util/fbDb.js");
+// const firebaseStoreDB = require("../util/fbDb.js");
 const residentRef = require("../util/fbDb");
 const handleText = async (message, replyToken, source) => {
   switch (message.text) {
@@ -11,12 +11,6 @@ const handleText = async (message, replyToken, source) => {
         .where("userId", "==", source.userId)
         .get()
         .then((res) => {
-          // if (res.empty == true) {
-          //   firebaseStoreDB
-          //     .collection("residents")
-          //     .doc(residentData.userId)
-          //     .set(residentData);
-          // }
           console.log(14, res);
         });
   }
