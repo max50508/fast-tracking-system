f42636b10b3b5394c57fc78ad5c86d3f8a42be31
yuncheng <h1253567@gmail.com>
2022-01-05 23:33:53 +0800

    update 同意

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -1,18 +1,18 @@
 const client = require("../config/client");
 const checkMessage = require("../util/message.js");
-// const firebaseStoreDB = require("../util/fbDb.js");
-const residentRef = require("../util/fbDb");
+const firebaseStoreDB = require("../util/fbDb.js");
+const residentRef = firebaseStoreDB.collection("residents");
 const handleText = async (message, replyToken, source) => {
   switch (message.text) {
     case "你好啊!":
       return await client.replyMessage(replyToken, checkMessage);
     default:
-    // residentRef
-    //   .where("userId", "==", source.userId)
-    //   .get()
-    //   .then((res) => {
-    //     console.log(14, res);
-    //   });
+      residentRef
+        .where("userId", "==", source.userId)
+        .get()
+        .then((res) => {
+          console.log(14, res);
+        });
   }
 };
 
--- fast-tracking-line-bot/util/fbDb.js
+++ fast-tracking-line-bot/util/fbDb.js
@@ -5,7 +5,5 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 const firebaseStoreDB = admin.firestore();
-const residentRef = firebaseStoreDB.collection("residents");
 
 module.exports = firebaseStoreDB;
-module.exports = residentRef;
