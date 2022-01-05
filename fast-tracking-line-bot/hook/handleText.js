376a3251b47b2d465dfc581e8d30ab80de8548ea
yuncheng <h1253567@gmail.com>
2022-01-06 01:20:47 +0800

    update 同意

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -17,9 +17,9 @@ const handleText = async (message, replyToken, source) => {
             console.log(14, val.data());
             Object.assign(residentData, val.data());
             if (val.data().privacy === false) {
-              return await client.replyMessage(replyToken, checkMessage);
+              return client.replyMessage(replyToken, checkMessage);
             } else {
-              return await client.replyMessage(replyToken, {
+              return client.replyMessage(replyToken, {
                 text: `感謝您同意! 現在可立即開始使用瞜! \n 到處探險吧 ! `,
               });
             }
