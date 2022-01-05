a8b3e2aac0748bfea95df66bc4761fb6c74caf7e
yuncheng <h1253567@gmail.com>
2022-01-05 21:14:13 +0800

    update line@ 歡迎訊息更新

--- fast-tracking-line-bot/hook/handleText.js
+++ fast-tracking-line-bot/hook/handleText.js
@@ -3,54 +3,69 @@ const client = require("../config/client");
 const handleText = async (message, replyToken, source) => {
   switch (message.text) {
     case "你好啊!":
-      return await client.replyMessage(replyToken, {
-        type: "flex",
-        altText: "This is a Flex Message",
-        contents: {
-          type: "bubble",
-          size: "kilo",
-          body: {
-            type: "box",
-            layout: "vertical",
-            contents: [
-              {
-                type: "text",
-                text: "為了提供個人化的服務，我會紀錄你告訴我的個人資訊，請詳細閱讀以上聲明，同意後我就能夠為你進行各項服務！",
-                weight: "regular",
-                size: "md",
-                decoration: "none",
-                align: "start",
-                wrap: true,
-                offsetTop: "none",
-                offsetStart: "xs",
-                offsetEnd: "xs",
-                offsetBottom: "none",
-              },
-            ],
-            backgroundColor: "#F7F7F7",
-            spacing: "none",
-            justifyContent: "space-around",
-          },
-          footer: {
-            type: "box",
-            layout: "vertical",
-            spacing: "sm",
-            contents: [
-              {
-                type: "button",
-                style: "link",
-                action: {
-                  type: "uri",
-                  label: "閱讀及管家注意事項及個資相關保護法同意協定",
-                  uri: "https://linecorp.com",
+      return await client.replyMessage(replyToken, [
+        {
+          type: "flex",
+          altText: "This is a Flex Message",
+          contents: {
+            type: "bubble",
+            size: "kilo",
+            body: {
+              type: "box",
+              layout: "vertical",
+              contents: [
+                {
+                  type: "text",
+                  text: "為了提供個人化的服務，我會紀錄你告訴我的個人資訊，請詳細閱讀以上聲明，同意後我就能夠為你進行各項服務！",
+                  weight: "regular",
+                  size: "md",
+                  decoration: "none",
+                  align: "start",
+                  wrap: true,
+                  offsetTop: "none",
+                  offsetStart: "xs",
+                  offsetEnd: "xs",
+                  offsetBottom: "none",
                 },
-                height: "sm",
+              ],
+              backgroundColor: "#F7F7F7",
+              spacing: "none",
+              justifyContent: "space-around",
+            },
+            footer: {
+              type: "box",
+              layout: "vertical",
+              spacing: "sm",
+              contents: [
+                {
+                  type: "button",
+                  style: "link",
+                  action: {
+                    type: "uri",
+                    label: "閱讀及管家注意事項及個資相關保護法同意協定",
+                    uri: "https://linecorp.com",
+                  },
+                  height: "sm",
+                },
+              ],
+              flex: 0,
+            },
+          },
+          quickReply: {
+            // ②
+            items: [
+              {
+                type: "action",
+                action: {
+                  type: "message",
+                  label: "同意",
+                  text: "同意",
+                },
               },
             ],
-            flex: 0,
           },
         },
-      });
+      ]);
     // {
     //   "type": "bubble",
     //   "size": "kilo",
