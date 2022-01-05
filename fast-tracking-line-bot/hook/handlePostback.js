11585e0eb427470f2e19b607ca03cc7b70eeeb6e
yuncheng <h1253567@gmail.com>
2022-01-06 00:35:43 +0800

    update 同意

--- fast-tracking-line-bot/hook/handlePostback.js
+++ fast-tracking-line-bot/hook/handlePostback.js
@@ -1,13 +1,14 @@
 const client = require("../config/client");
 // const checkMessage = require("../util/message.js");
-// const firebaseStoreDB = require("../util/fbDb.js");
-// const residentRef = firebaseStoreDB.collection("residents");
+const firebaseStoreDB = require("../util/fbDb.js");
+const residentRef = firebaseStoreDB.collection("residents");
 // const residentData = {};
 const handlePostback = async (event, replyToken) => {
-  console.log(8, event.postback.data);
-  // if (data === 'DATE' || data === 'TIME' || data === 'DATETIME') {
-  //   data += `(${JSON.stringify(event.postback.params)})`;
-  // }
+  console.log(8, event.postback.data, replyToken);
+  if (devent.postback.data === true) {
+    // data += `(${JSON.stringify(event.postback.params)})`;
+    console.log("nide");
+  }
 };
 
 module.exports = handlePostback;
